19cf5e010e47e6fc4def226906d83b5d8573343d
Zero <zerosoft@vip.qq.com>
2024-10-29 10:26:55 +0800

    Add log4j2 logging to UDP and TCP encoding/decoding
    Add detailed log4j2 logging to the UDP and TCP encoding and decoding processes.
    
    * **Dependencies**: Add `log4j-core` and `log4j-api` dependencies to `core/pom.xml`.
    * **Configuration**: Create `log4j2.xml` in `core/src/main/resources` with a basic log4j2 configuration.
    * **TCPPacketDecoder**: Import `org.apache.logging.log4j.Logger` and `org.apache.logging.log4j.LogManager`. Add a `private static final Logger logger` field. Add debug logs to trace the decoding process in the `decode` method.
    * **TCPPacketEncoder**: Import `org.apache.logging.log4j.Logger` and `org.apache.logging.log4j.LogManager`. Add a `private static final Logger logger` field. Add debug logs to trace the encoding process in the `encode` method.
    * **UDPPacketDecoder**: Import `org.apache.logging.log4j.Logger` and `org.apache.logging.log4j.LogManager`. Add a `private static final Logger logger` field. Add debug logs to trace the decoding process in the `decode` method.
    * **UDPPacketEncoder**: Import `org.apache.logging.log4j.Logger` and `org.apache.logging.log4j.LogManager`. Add a `private static final Logger logger` field. Add debug logs to trace the encoding process in the `encode` method.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/zerosoft/avolution?shareId=XXXX-XXXX-XXXX-XXXX).

--- core/src/main/java/com/avolution/net/tcp/codec/TCPPacketEncoder.java
+++ core/src/main/java/com/avolution/net/tcp/codec/TCPPacketEncoder.java
@@ -5,8 +5,12 @@ import com.avolution.net.tcp.TCPPacket;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class TCPPacketEncoder extends MessageToByteEncoder<MessagePacket> {
+
+    private static final Logger logger = LogManager.getLogger(TCPPacketEncoder.class);
 
     @Override
     protected void encode(ChannelHandlerContext ctx, MessagePacket msg, ByteBuf out) throws Exception {
@@ -20,5 +24,9 @@ public class TCPPacketEncoder extends MessageToByteEncoder<MessagePacket> {
         out.writeInt(((TCPPacket) msg).getProtocolId());
         // 写入包体内容
         out.writeBytes(msg.getContent());
+
+        // Add debug logs to trace the encoding process
+        logger.debug("Encoded TCPPacket: protocolType={}, encryptionType={}, protocolId={}, content={}",
+                ((TCPPacket) msg).getProtocolType(), ((TCPPacket) msg).getEncryptionType(), ((TCPPacket) msg).getProtocolId(), new String(msg.getContent()));
     }
 }
--- core/src/main/java/com/avolution/net/udp/codec/UDPPacketEncoder.java
+++ core/src/main/java/com/avolution/net/udp/codec/UDPPacketEncoder.java
@@ -5,8 +5,12 @@ import com.avolution.net.udp.UDPPacket;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class UDPPacketEncoder extends MessageToByteEncoder<MessagePacket> {
+
+    private static final Logger logger = LogManager.getLogger(UDPPacketEncoder.class);
 
     @Override
     protected void encode(ChannelHandlerContext ctx, MessagePacket msg, ByteBuf out) throws Exception {
@@ -18,5 +22,9 @@ public class UDPPacketEncoder extends MessageToByteEncoder<MessagePacket> {
         out.writeInt(((UDPPacket) msg).getAcknowledgmentNumber());
         // Write the packet content
         out.writeBytes(msg.getContent());
+
+        // Add debug logs to trace the encoding process
+        logger.debug("Encoded UDPPacket: sequenceNumber={}, acknowledgmentNumber={}, content={}",
+                ((UDPPacket) msg).getSequenceNumber(), ((UDPPacket) msg).getAcknowledgmentNumber(), new String(msg.getContent()));
     }
 }
